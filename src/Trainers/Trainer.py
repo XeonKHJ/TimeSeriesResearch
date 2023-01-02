daa39916c3a5fe18e4fe8b568d15888236ced486
康宏嘉 <redalertkhj@live.cn>
2023-01-03 02:55:37 +0800

    remove l2norm in regular trainer.

--- src/Trainers/Trainer.py
+++ src/Trainers/Trainer.py
@@ -5,7 +5,7 @@ class Trainer(ITrainer):
     def __init__(self, model):
         self.mlModel = model
         self.lossFunc = torch.nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.mlModel.parameters(), lr=1e-2, weight_decay=0.001)
+        self.optimizer = torch.optim.Adam(self.mlModel.parameters(), lr=1e-2)
 
     def train(self, trainSet, trainSetLength, labelSet):
         output = self.mlModel(trainSet, trainSetLength)
