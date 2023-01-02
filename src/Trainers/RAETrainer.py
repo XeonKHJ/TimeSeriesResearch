84705602bf9548fc0d0711c0ae190c735e903325
康宏嘉 <redalertkhj@live.cn>
2023-01-03 02:55:57 +0800

    feat: impl RAETrainer.

--- src/Trainers/RAETrainer.py
+++ src/Trainers/RAETrainer.py
@@ -1,22 +1,28 @@
 from Trainers.ITrainer import ITrainer
+import torch
 
 class RAETrainer(ITrainer):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, mlModel):
+        self.mlModel = mlModel
+        self.lossFunc = self.lossFunc = torch.nn.MSELoss()
+        self.ts = 0
+        self.tsLambda = 0.0001
+        self.optimizer = torch.optim.Adam(self.mlModel.parameters(), lr=1e-2, weight_decay=0.001)
 
-    def train(self, trainSet, labelSet):
-        tl = t - ts
-        if datasetSize - batchSize == 0:
-            startIdx = 0
-        else:
-            startIdx = currentIdx % (datasetSize - batchSize)
-        endIdx = startIdx + batchSize
-        currentIdx += batchSize 
-        trainSet = toTrainDataset[startIdx:endIdx]
-        output = mlModel(trainSet, labelSet)
-        labelTensor = torch.full(output.shape, 1.0)
-        loss = lossFunc(output, labelDataset[startIdx:endIdx])
+    def train(self, trainSet, trainSetLength, labelSet):
+        self.tl = trainSet - self.ts
+
+        output = self.mlModel(trainSet, trainSetLength)
+        loss = self.lossFunc(output, self.tl)
+        self.optimizer.backward()
+
+        lambdaTs1 = self.tsLambda * torch.sum(torch.abs(self.ts))
+        loss = loss + lambdaTs1
+
+        self.tl = self.mlModel(self.tl, trainSetLength)
 
         loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        return loss
+        
