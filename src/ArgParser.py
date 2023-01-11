57e51ff64597f63cf5e58c37996b187ba41f601a
Kang Hongjia <redalertkhj@live.cn>
2023-01-11 05:57:30 +0000

    fix: only parse known args for vscode notebook avaliablity.

--- src/ArgParser.py
+++ src/ArgParser.py
@@ -4,5 +4,5 @@ def getArgs():
     argParser = argparse.ArgumentParser()
     
     argParser.add_argument("--disablePlot", help="increase output verbosity", action='store_true')
-    args = argParser.parse_args()
+    args, opt = argParser.parse_known_args()
     return args
