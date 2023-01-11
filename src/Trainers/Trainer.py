94d66d5e8e5b012e8e83fdae518aaaa5af0a3ba5
Kang Hongjia <redalertkhj@live.cn>
2023-01-11 05:00:17 +0000

    feat: add time info when trainning.

--- src/Trainers/Trainer.py
+++ src/Trainers/Trainer.py
@@ -1,16 +1,21 @@
 from Trainers.ITrainer import ITrainer
+import time
 import torch
 
 class Trainer(ITrainer):
     def __init__(self, model):
         self.mlModel = model
         self.lossFunc = torch.nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.mlModel.parameters(), lr=1e-5)
+        self.optimizer = torch.optim.Adam(self.mlModel.parameters(), lr=1e-3)
 
     def train(self, trainSet, trainSetLength, labelSet):
+        startTime = time.perf_counter()
         output = self.mlModel(trainSet, trainSetLength)
+        outputedTime = time.perf_counter()
         loss = self.lossFunc(output, labelSet)
         loss.backward()
         self.optimizer.step()
         self.optimizer.zero_grad()
+        backwardTime = time.perf_counter()
+        print("\tloss\t", loss.item(), "\tforward\t", outputedTime-startTime, "\tlosstime\t", backwardTime-outputedTime)
         return loss
