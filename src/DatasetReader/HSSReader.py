779bbedfe6875bbaee4c3efcb54e04b794c3f3ab
康宏嘉 <redalertkhj@live.cn>
2023-01-25 17:23:20 +0800

    feat: allow loading normal/abnormal hss data.

--- src/DatasetReader/HSSReader.py
+++ src/DatasetReader/HSSReader.py
@@ -5,9 +5,10 @@ import torch
 from DatasetReader.DatasetReader import IDatasetReader
 
 class HSSReader(IDatasetReader):
-    def __init__(self, folderPath) -> None:
+    def __init__(self, folderPath, isNormal=True) -> None:
         super().__init__()
         self.folderPath = folderPath
+        self.isNormal = isNormal
 
     def read(self):
         fileList = os.listdir(self.folderPath)
@@ -16,15 +17,19 @@ class HSSReader(IDatasetReader):
         featureSize = 20
         maxDataLength = 0
         for file in fileList:
-            columnData = list()
-            rowData = list()
-            filePath = os.path.join(self.folderPath, file)
-            data = pandas.read_csv(filePath)
-            columns = data.columns.tolist()
-            for column in columns:
-                columnData.append(data[column].to_list())
-            fulldata.append(columnData)
-            maxDataLength = max(len(columnData[0]), maxDataLength)
+            if self.isNormal:
+                toRead = file.__contains__('normal')
+            else:
+                toRead = file.__contains__('anomalous')
+            if toRead:
+                columnData = list()
+                filePath = os.path.join(self.folderPath, file)
+                data = pandas.read_csv(filePath)
+                columns = data.columns.tolist()
+                for column in columns:
+                    columnData.append(data[column].to_list())
+                fulldata.append(columnData)
+                maxDataLength = max(len(columnData[0]), maxDataLength)
         fulldata.sort(key=(lambda elem:len(elem)), reverse=True)
         dataTensor = torch.zeros([fulldata.__len__(), maxDataLength, featureSize])
         # dataTensor = torch.tensor(fulldata)
