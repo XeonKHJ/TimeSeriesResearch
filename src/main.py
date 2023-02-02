565a40249f77932f9f33630acadddbd3a6f2b789
Kang Hongjia <redalertkhj@live.cn>
2023-02-02 10:09:59 +0000

    change some config.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn
 import os.path as path
+from DatasetReader.HSSReader import HSSReader
+from DatasetReader.SegmentNABDataReader import SegmentNABDataReader
 from DatasetReader.SingleNABDataReader import SingleNABDataReader
 from TaskConfig.CorrectTaskConfig import CorrectTaskConfig
 from TaskConfig.GruAEConfig import GruAEConfig
+from TaskConfig.OffsetGruAEConfig import OffsetGruAEConfig
 from TaskConfig.RAECorrectorTaskConfig import RAECorrectorTaskConfig
 from TaskConfig.RAECorrectorWithTrendTaskConfig import RAECorrectorWithTrendTaskConfig
 
@@ -12,31 +15,44 @@ from TaskConfig.RAETaskConfig import RAETaskConfig
 from DatasetReader.NABReader import NABReader
 import ArgParser
 from TaskConfig.OneDAutoencoderConfig import OneDAutoencoderConfig
+from TaskConfig.StaticAeConfig import StaticAeConfig
 from Trainers.CorrectorTrainer import CorrectorTrainer
 
-# normalDataReader = SingleNABDataReader("../datasets/preprocessed/NAB/artificialNoAnomaly/artificialNoAnomaly/art_daily_small_noise.csv")
-normalDataReader = NABReader("../datasets/preprocessed/NAB/artificialNoAnomaly/artificialNoAnomaly")
+# normalDataReader = NABReader("../datasets/preprocessed/NAB/artificialNoAnomaly/artificialNoAnomaly")
+normalDataReader = SegmentNABDataReader("../datasets/preprocessed/NAB/artificialNoAnomaly/artificialNoAnomaly/art_daily_small_noise.csv")
+# normalDataReader = HSSReader("../datasets/preprocessed/HSS")
+# abnormalDataReader = HSSReader("../datasets/preprocessed/HSS", isNormal=False)
 abnormalDataReader = NABReader("../datasets/preprocessed/NAB/artificialWithAnomaly/artificialWithAnomaly")
 # abnormalDataReader = SingleNABDataReader("../datasets/preprocessed/NAB/artificialWithAnomaly/artificialWithAnomaly/art_daily_jumpsup.csv")
 # skabDataReader = SKABDatasetReader("C:\\Users\\redal\\source\\repos\\SKAB\\data\\valve1")
 modelFolderPath = "SavedModels"
 
 if __name__ == '__main__':
+    if torch.cuda.is_available():
+        print("CUDA is avaliable.")
+    else:
+        print("CUDA is unavaliable")
+
     # read arg
     args = ArgParser.getArgs()
     isLoggerEnable = not (args.disablePlot)
-    # config = RAETaskConfig(modelFolderPath, isLoggerEnable)
+
+    # load data
+    normalDataset, normalDatasetLengths, normalLabels = normalDataReader.read()
+    abnormalDataset, abnormalDatasetLengths, abnormalLabels = abnormalDataReader.read()
+
+    config = RAETaskConfig(modelFolderPath, isLoggerEnable)
     # config = RAECorrectorTaskConfig(modelFolderPath)
     # config = OneDAutoencoderConfig(modelFolderPath, isLoggerEnable)
+    # config = StaticAeConfig(modelFolderPath, isLoggerEnable)
     # config = RAECorrectorWithTrendTaskConfig(modelFolderPath, isLoggerEnable)
-    config = GruAEConfig(modelFolderPath, isLoggerEnable)
+    # config = OffsetGruAEConfig(modelFolderPath, isLoggerEnable, len(normalDataset[0][0]), len(normalDataset[0][0]))
+    # config = GruAEConfig(modelFolderPath, isLoggerEnable)
 
     # load config
     mlModel, datasetSeperator, trainer, logger, dataNormalizer, taskName = config.getConfig()
 
-    # load data
-    normalDataset, normalDatasetLengths = normalDataReader.read()
-    abnormalDataset, abnormalDatasetLengths = abnormalDataReader.read()
+
     dataNormalizer.addDatasetToRef(normalDataset)
     dataNormalizer.addDatasetToRef(abnormalDataset)
     normalDataset = dataNormalizer.normalizeDataset(normalDataset)
@@ -66,7 +82,7 @@ if __name__ == '__main__':
         labelSetLengths = labelDatasetLengths[startIdx:endIdx]
         loss = trainer.train(trainSet, labelSetLengths, labelSet)
         if epoch % 100 == 0:
-            trainer.evalResult(normalDataset, normalDatasetLengths, 'normalset')
+            # trainer.evalResult(normalDataset, normalDatasetLengths, 'normalset')
             trainer.evalResult(abnormalDataset, abnormalDatasetLengths, 'abnormalset')
             trainer.save()
         epoch += 1
