f8d6274104ccbf760c4b7ad0d921810f1fa04cb0
Kang Hongjia <redalertkhj@live.cn>
2023-02-09 17:57:57 +0000

    feat add aheadtask config and trainer.

---
+++ src/TaskConfig/AheadTaskConfig.py
@@ -0,0 +1,38 @@
+from Network.GruAutoencoder import GruAutoencoder
+from Logger.PlotLogger import PlotLogger
+from TaskConfig.ITaskConfig import ITaskConfig
+from Trainers.AheadTrainer import AheadTrainer
+from DataNormalizer.DataNormalizer import DataNormalizer
+from DataSeperator.NoSepDataSeperator import NoSepDataSeperator
+
+import torch
+import os.path as path
+
+class AheadTaskConfig(ITaskConfig):
+    def __init__(self, modelFolderPath, isLogEnable, inputFeatureCount=1, outputFeatureCount=1, fileList=[]):
+        self.modelFolderPath = modelFolderPath
+        self.isLogEnable = isLogEnable
+        self.inputFeatureSize = inputFeatureCount
+        self.outputFeatureSize = outputFeatureCount
+        self.fileList = fileList
+
+    def getConfig(self, isCuda = False):
+        feature_size = self.inputFeatureSize
+        output_size = self.outputFeatureSize
+        logger = PlotLogger(isPlotEnable=self.isLogEnable)
+        mlModel = GruAutoencoder(feature_size,4,output_size,2, 0)
+        taskName = 'AheadTaskConfig'
+        try:
+            mlModel.load_state_dict(torch.load(path.join(self.modelFolderPath, taskName + ".pt")))
+        except:
+            pass
+        if torch.cuda.is_available():
+            mlModel.cuda()
+        # trainer = RAETrainer(mlModel, logger, taskName, 10)
+        trainer = AheadTrainer(mlModel, taskName, logger, 1e-3, self.fileList)
+        datasetSeperator = NoSepDataSeperator()
+        # logger = PlotLogger()
+        
+        dataNormalizer = DataNormalizer()
+        
+        return mlModel, datasetSeperator, trainer, logger, dataNormalizer, taskName
--- src/Trainers/AheadTrainer.py
+++ src/Trainers/AheadTrainer.py
@@ -3,7 +3,7 @@ import time
 import torch
 import os.path as path
 
-class Trainer(ITrainer):
+class AheadTrainer(ITrainer):
     def __init__(self, model, taskName, logger, learningRate=1e-3, fileList=[]):
         self.mlModel = model
         self.lossFunc = torch.nn.MSELoss()
@@ -12,18 +12,37 @@ class Trainer(ITrainer):
         self.logger = logger
         self.fileList = fileList
         self.modelFolderPath = "SavedModels"
+        self.windowSize = 100
+        self.splitData = None
 
     def train(self, trainSet, trainSetLength, labelSet):
         self.mlModel.train()
         startTime = time.perf_counter()
-        output = self.mlModel(trainSet, trainSetLength)
-        outputedTime = time.perf_counter()
-        loss = self.lossFunc(output, trainSet)
-        loss.backward()
-        self.optimizer.step()
-        self.optimizer.zero_grad()
-        backwardTime = time.perf_counter()
-        print("\tloss\t", loss.item(), "\tforward\t", outputedTime-startTime, "\tlosstime\t", backwardTime-outputedTime)
+        windowSize = self.windowSize
+        
+        if self.splitData == None:
+            self.splitedData = list()
+            # split
+            for idx in range(trainSet.shape[0]):
+                splitdata = list()
+                for timeIdx in range(trainSet.shape[1] - self.windowSize + 1):
+                    splitdata.append(trainSet[idx, timeIdx:timeIdx+self.windowSize, :])
+                self.splitedData.append(torch.cat(splitdata).reshape((-1, self.windowSize, trainSet.shape[2])))
+            # self.splitedData = torch.cat(self.splitedData)
+
+        for setIdx in range(trainSet.shape[0]):
+            lengths = torch.tensor(windowSize).repeat(self.splitedData[setIdx].shape[0]).tolist()
+            output = self.mlModel(self.splitedData[setIdx], lengths)
+            outputedTime = time.perf_counter()
+            output = output[0:output.shape[0]-self.windowSize]
+            output = torch.cat((self.splitedData[setIdx][0:self.windowSize], output))
+            loss = self.lossFunc(output, self.splitedData[setIdx])
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            backwardTime = time.perf_counter()
+            print("\tloss\t", loss.item(), "\tforward\t", outputedTime-startTime, "\tlosstime\t", backwardTime-outputedTime)
+
         return loss
 
     def evalResult(self, validDataset, validsetLengths, storeName=None):
@@ -53,9 +72,16 @@ class Trainer(ITrainer):
         torch.save(self.mlModel.state_dict(), path.join(self.modelFolderPath, filename))
 
     def reconstruct(self, mlModel, validDataset, validsetLength):
-        reconstructSeqs = []
-        for idx in range(0, validDataset.shape[0], 100):
-            output = mlModel(validDataset[:,idx:idx+100,:])
-            reconstructSeqs.append(output)
-        reconstructed = torch.cat(reconstructSeqs, 1)
-        return reconstructed
+        reconstructSeqs = torch.zeros(validDataset.shape, device=torch.device('cuda'))
+        preIdx = -100
+        for idx in range(0, validDataset.shape[1] - self.windowSize, self.windowSize):
+            if idx+2*self.windowSize > reconstructSeqs.shape[1]:
+                break
+            lengths = torch.tensor(self.windowSize).repeat(validDataset.shape[0]).tolist()
+            reconstructSeqs[:,idx+self.windowSize:idx+2*self.windowSize,:] = mlModel(validDataset[:,idx:idx+self.windowSize,:], lengths)
+            preIdx = idx
+            
+        lengths = torch.tensor(self.windowSize).repeat(validDataset.shape[0]).tolist()
+        reconstructSeqs[:,reconstructSeqs.shape[1]-self.windowSize:reconstructSeqs.shape[1],:] = mlModel(validDataset[:,validDataset.shape[1]-2*self.windowSize:validDataset.shape[1]-self.windowSize:,:], lengths)
+        reconstructSeqs[:,0:self.windowSize, :] = validDataset[:, 0:self.windowSize, :]
+        return reconstructSeqs
