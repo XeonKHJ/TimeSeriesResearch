441640424dca8aa9e50b49f52048ea8242778b99
Kang Hongjia <redalertkhj@live.cn>
2023-02-11 13:41:56 +0000

    feat: add 1d-gru-ae on nab-aws datset experiment.

---
+++ src/Experiment/OneDGruAENABAwsExperiment.py
@@ -0,0 +1,29 @@
+from DataNormalizer.DataNormalizer import DataNormalizer
+from DataSeperator.NoSepDataSeperator import NoSepDataSeperator
+from DatasetReader.NABFoldersReader import NABFoldersReader
+from TaskConfig.OneDAutoencoderConfig import OneDAutoencoderConfig
+
+from globalConfig import globalConfig
+
+from DataProcessor.ShuffleDataProcessor import ShuffleDataProcessor
+from DataProcessor.SlidingWindowStepDataProcessor import SlidingWindowStepDataProcessor
+
+class OneDGruAENABAwsExperiment(object):
+    def __init__(self, logger):
+        self.logger = logger
+
+    def getName(self):
+        return "OneDGruAENABAws"
+
+    def getExperimentConfig(self):
+        dataReader = NABFoldersReader("../../NAB/", "realAWSCloudwatch")
+        displayDataReader = NABFoldersReader("../../NAB/", "realAWSCloudwatch")
+        config = OneDAutoencoderConfig(globalConfig.getModelPath(), self.logger, self.getName(), showTrainingInfo=True)
+        trainer = config.getConfig()
+        processers = [
+            SlidingWindowStepDataProcessor(windowSize=100, step=20),
+            ShuffleDataProcessor()
+        ]
+        datasetSeperator = NoSepDataSeperator()        
+        dataNormalizer = DataNormalizer()
+        return trainer, dataReader, processers, datasetSeperator, dataNormalizer
